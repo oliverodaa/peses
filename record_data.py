1db7c0469683d5c9bf99b94eec8030e8004ff125
Ollie O'Donnell <oliverodaa@gmail.com>
2017-04-08 15:07:41 -0700

    comment out GPIO references so that it can run on any computer

--- record_data.py
+++ record_data.py
@@ -4,55 +4,56 @@ import os
 import csv
 import random
 import argparse
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if ((adcnum > 7) or (adcnum < 0)):
-            return -1
-    GPIO.output(cspin, True)
-    GPIO.output(clockpin, False)  # start clock low
-    GPIO.output(cspin, False)     # bring CS low
-    commandout = adcnum
-    commandout |= 0x18  # start bit + single-ended bit
-    commandout <<= 3    # we only need to send 5 bits here
-    for i in range(5):
-            if (commandout & 0x80):
-                    GPIO.output(mosipin, True)
-            else:
-                    GPIO.output(mosipin, False)
-            commandout <<= 1
-            GPIO.output(clockpin, True)
-            GPIO.output(clockpin, False)
-    adcout = 0
-    # read in one empty bit, one null bit and 10 ADC bits
-    for i in range(12):
-            GPIO.output(clockpin, True)
-            GPIO.output(clockpin, False)
-            adcout <<= 1
-            if (GPIO.input(misopin)):
-                    adcout |= 0x1
-    GPIO.output(cspin, True)
-    adcout >>= 1       # first bit is 'null' so drop it
-    return adcout
+    return 0
+    # if ((adcnum > 7) or (adcnum < 0)):
+    #         return -1
+    # GPIO.output(cspin, True)
+    # GPIO.output(clockpin, False)  # start clock low
+    # GPIO.output(cspin, False)     # bring CS low
+    # commandout = adcnum
+    # commandout |= 0x18  # start bit + single-ended bit
+    # commandout <<= 3    # we only need to send 5 bits here
+    # for i in range(5):
+    #         if (commandout & 0x80):
+    #                 GPIO.output(mosipin, True)
+    #         else:
+    #                 GPIO.output(mosipin, False)
+    #         commandout <<= 1
+    #         GPIO.output(clockpin, True)
+    #         GPIO.output(clockpin, False)
+    # adcout = 0
+    # # read in one empty bit, one null bit and 10 ADC bits
+    # for i in range(12):
+    #         GPIO.output(clockpin, True)
+    #         GPIO.output(clockpin, False)
+    #         adcout <<= 1
+    #         if (GPIO.input(misopin)):
+    #                 adcout |= 0x1
+    # GPIO.output(cspin, True)
+    # adcout >>= 1       # first bit is 'null' so drop it
+    # return adcout
 
 def readadc_with_settings():
     # change these as desired - they're the pins connected from the
     # SPI port on the ADC to the pins on the Raspberry Pi
     if (True):
         return random.gauss(500, 2)
-    SPICLK = 18
-    SPIMISO = 23
-    SPIMOSI = 24
-    SPICS = 25
-    # set up the SPI interface pins
-    GPIO.setup(SPIMOSI, GPIO.OUT)
-    GPIO.setup(SPIMISO, GPIO.IN)
-    GPIO.setup(SPICLK, GPIO.OUT)
-    GPIO.setup(SPICS, GPIO.OUT)
-    # 10k trim pot connected to adc #0
-    adcnum = 0
-    return readadc(adcnum, SPICLK, SPIMOSI, SPIMISO, SPICS)
+    # SPICLK = 18
+    # SPIMISO = 23
+    # SPIMOSI = 24
+    # SPICS = 25
+    # # set up the SPI interface pins
+    # GPIO.setup(SPIMOSI, GPIO.OUT)
+    # GPIO.setup(SPIMISO, GPIO.IN)
+    # GPIO.setup(SPICLK, GPIO.OUT)
+    # GPIO.setup(SPICS, GPIO.OUT)
+    # # 10k trim pot connected to adc #0
+    # adcnum = 0
+    # return readadc(adcnum, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
 # ~~~~~~~ Time Helpers ~~~~~~~~~~~~~
 
@@ -136,7 +137,7 @@ def record_data(NUM_MEASUREMENTS, SLEEP_TIME, SAVE_FILE_NAME, MAX_TIME, SUBTRACT
 
 
 def main():
-    GPIO.setmode(GPIO.BCM)
+    # GPIO.setmode(GPIO.BCM)
     # ~~~~~~~ OPTIONS TO CONFIGURE ~~~~~~~~~
     num_before_threshold = 20
     num_measurements = 1000
